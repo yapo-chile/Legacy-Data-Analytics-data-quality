73fb858cf34cfb39f307c8786968eb91e521be2a
Juan José Figueira <juanjosefigueira@Juans-MacBook-Pro.local>
2020-06-23 11:46:56 -0400

    Delete time_series_data_quality directory

--- time_series_data_quality/scripts/commands/docker-publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Publishing docker image to Artifactory"
-docker login --username "${ARTIFACTORY_USER}" --password "${ARTIFACTORY_PWD}" "${DOCKER_REGISTRY}"
-docker push "${DOCKER_IMAGE}"
